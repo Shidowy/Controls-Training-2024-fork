b01352ca8dd79272a175c7bafca6c1d23305b41e
Shidowy <101477459+Shidowy@users.noreply.github.com>
2024-10-17 18:45:27 -0700

    final1

--- src/main/java/frc/robot/commands/NoteHandlingCommandBuilder.java
+++ src/main/java/frc/robot/commands/NoteHandlingCommandBuilder.java
@@ -1,14 +1,8 @@
 package frc.robot.commands;
-
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.NoteHandling.GeneralRoller;
 import frc.robot.subsystems.NoteHandling.GeneralRoller.GeneralRollerStates;
-
-import java.util.function.Supplier;
 
 // We are gonna be writing commands today, use this doc to progress through the training:
 // https://docs.google.com/document/d/1iaDaoYRCIEgX1hY3d1hFmAgDCdyEqP5SY_TVuxs04kA/edit?tab=t.0
@@ -26,5 +20,5 @@ public class NoteHandlingCommandBuilder {
     public static Command generalRollerStop(GeneralRoller generalRoller) {
         return new InstantCommand(() -> generalRoller.requestState(GeneralRollerStates.StateOff), generalRoller);
     }    
-    
+
 }
