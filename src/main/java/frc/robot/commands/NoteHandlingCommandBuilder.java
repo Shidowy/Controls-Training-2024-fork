c5ff1e93166d50d24993c19eb0f298b879618f6d
Shidowy <101477459+Shidowy@users.noreply.github.com>
2024-10-17 18:41:27 -0700

    push 2

--- src/main/java/frc/robot/commands/NoteHandlingCommandBuilder.java
+++ src/main/java/frc/robot/commands/NoteHandlingCommandBuilder.java
@@ -1,25 +1,23 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.NoteHandling.GeneralRoller;
+import frc.robot.subsystems.NoteHandling.GeneralRoller.GeneralRollerStates;
+
 import java.util.function.Supplier;
-import frc.robot.subsystems.generalRollerRunForward;
 
 // We are gonna be writing commands today, use this doc to progress through the training:
 // https://docs.google.com/document/d/1iaDaoYRCIEgX1hY3d1hFmAgDCdyEqP5SY_TVuxs04kA/edit?tab=t.0
 
 public class NoteHandlingCommandBuilder {
 
-    public static Command generalRollerRunForward(/* add parameters here, you will need something pretty important */){
-
-        // Instantiate a command/command group here, then return it. Remember to use the keyword "new" when instantiating an object
-
-        return /* command */;
-
+    public static Command generalRollerRunForward(GeneralRoller generalRoller) {
+        return new InstantCommand(() -> generalRoller.requestState(GeneralRollerStates.StateForward), generalRoller);
     }
-
-    // Repeat above for the other 2 commands specified in the doc
+    
 
 }
