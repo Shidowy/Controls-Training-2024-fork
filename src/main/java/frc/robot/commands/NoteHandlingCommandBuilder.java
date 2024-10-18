cccb8d974329da053932ca1acef4a2040fcafdb9
Shidowy <101477459+Shidowy@users.noreply.github.com>
2024-10-17 18:27:48 -0700

    push 1

--- src/main/java/frc/robot/commands/NoteHandlingCommandBuilder.java
+++ src/main/java/frc/robot/commands/NoteHandlingCommandBuilder.java
@@ -1,6 +1,11 @@
-
 package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import java.util.function.Supplier;
+import frc.robot.subsystems.generalRollerRunForward;
 
 // We are gonna be writing commands today, use this doc to progress through the training:
 // https://docs.google.com/document/d/1iaDaoYRCIEgX1hY3d1hFmAgDCdyEqP5SY_TVuxs04kA/edit?tab=t.0
