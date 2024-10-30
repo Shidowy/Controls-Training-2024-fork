ab6be9aaa731c5a3659820d0d20a8f1d7f5b708b
Shidowy <101477459+Shidowy@users.noreply.github.com>
2024-10-29 18:37:53 -0700

    base

---
+++ src/main/java/frc/robot/subsystems/NoteHandling/Intake.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems.NoteHandling;
+import edu.wpi.first.math.controller.PIDController;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import static frc.robot.Constants.IntakeConstants.*;
+import static frc.robot.Constants.*;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+public class Intake extends SubsystemBase {
+  //CODE
+}
