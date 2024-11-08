093350de0786e23ffb91cc9f14e5c0b05fafe095
Shidowy <101477459+Shidowy@users.noreply.github.com>
2024-11-07 19:13:23 -0800

    ffff

--- src/main/java/frc/robot/subsystems/NoteHandling/Shooter.java
+++ src/main/java/frc/robot/subsystems/NoteHandling/Shooter.java
@@ -1,8 +1,10 @@
-package frc.robot.subsystems;
+package frc.robot.subsystems.NoteHandling;
+
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+// Imports go here
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import static frc.robot.Constants.ShooterConstants.*;
@@ -16,94 +18,92 @@ import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.ctre.phoenix6.controls.*;
 
+// FOLLOW ALONG THIS DOCUMENTATION: https://docs.google.com/document/d/143tNsvYQFAErQTJDxO9d1rwM7pv80vpLfLK-WiIEOiw/edit?tab=t.0
+
 public class Shooter extends SubsystemBase {
 
-    public enum ShooterStates {
+    public enum ShooterStates{
         OFF,
         LOW_GOAL,
+        MID_GOAL,
         HIGH_GOAL,
         TRANSITION
     }
 
-    public static ShooterStates m_shooterRequestedState = ShooterStates.OFF;
-    public static ShooterStates m_shooterCurrentState = ShooterStates.OFF;
- 
-    private TalonFX leftShooterMotor;
-    private TalonFX rightShooterMotor;
+    public static ShooterStates m_shooterRequestedState;
+    public static ShooterStates m_shooterCurrentState;
 
-    private PIDController pidController;
-    
+    private final TalonFX leftShooterMotor;
+    private final TalonFX rightShooterMotor
+
     private double desiredVelocity = 0;
     private double desiredVoltage = 0;
 
     public Shooter(DoubleSupplier distanceFromSpeaker) {
 
-        leftShooterMotor = new TalonFX(kShooterLeftPort, "ShooterControlLoop");
-        rightShooterMotor = new TalonFX(kShooterRightPort, "ShooterControlLoop");
+        leftShooterMotor = new TalonFX(LEFT_SHOOTER_MOTOR_ID);
+        rightShooterMotor = new TalonFX(RIGHT_SHOOTER_MOTOR_ID);
 
-        TalonFXConfiguration talonFXConfigs = new TalonFXConfiguration();
+        var talonFXConfigs = new TalonFXConfiguration();
+
+        talonFXConfigs.neutralDeadband = 0.001;
         talonFXConfigs.voltageCompSaturation = 12.0;
-        talonFXConfigs.neutralDeadband = 0.01;
+        talonFXConfigs.neutralMode = NeutralModeValue.Coast;
         leftShooterMotor.getConfigurator().apply(talonFXConfigs);
         rightShooterMotor.getConfigurator().apply(talonFXConfigs);
 
-        m_shooterCurrentState = ShooterStates.OFF;
 
-        pidController = new PIDController(0.1, 0.0, 0.0); 
+        // give some default state to these guys
+        // m_shooterCurrentState;
+        // m_shooterRequestedState;
+
     }
-
+        
     @Override
     public void periodic() {
-        if (m_shooterRequestedState != m_shooterCurrentState) {
-            handleStateChange();
-        }
 
+
+        // SWITCH/IF STATEMENT GOES HERE
+
+        // ||||||||||||||||||||||||||||||||
+     
         runControlLoop();
+    
+        // ERROR CHECKING GOES HERE
+
+        // ||||||||||||||||||||||||||||||||
+
     }
 
-    public void handleStateChange() {
-        switch (m_shooterRequestedState) {
-            case OFF:
-                leftShooterMotor.set(ControlMode.PercentOutput, 0);
-                rightShooterMotor.set(ControlMode.PercentOutput, 0);
-                break;
-            case LOW_GOAL:
-                desiredVelocity = 1500; 
-                break;
-            case HIGH_GOAL:
-                desiredVelocity = 3000;
-                break;
-            case TRANSITION:
-                break;
-        }
+      public void runControlLoop() {
+        // SHOOTER SHENANIGANS GO HERE UNLESS YOU ARE TOO COOL FOR THAT
 
-        m_shooterCurrentState = m_shooterRequestedState;
+        // ||||||||||||||||||||||||||||||||
+      }
+    
+      // SO MANY METHODS TO MAKE (like 4), SO LITTLE TIME TO DO IT (literally 6 hours)
+
+      public double getVelocity() {
+        // CHANGE DIS PLZ
+        return 0;
+      }
+    
+      public double getError() {
+        // CHANGE DIS PLZ
+        return 0;
+      }
+     
+      public void requestState(ShooterStates requestedState) {
+        // CHANGE DIS PLZ
+      }
+     
+      public ShooterStates getCurrentState() {
+        // CHANGE DIS PLZ
+        return null;
+      }
+
+        // ||||||||||||||||||||||||||||||||
+
     }
 
-    public void runControlLoop() {
-
-        double currentVelocity = getVelocity();
-        double velocityError = desiredVelocity - currentVelocity;
-
-        double pidOutput = pidController.calculate(currentVelocity, desiredVelocity);
-
-        leftShooterMotor.set(ControlMode.PercentOutput, pidOutput);
-        rightShooterMotor.set(ControlMode.PercentOutput, pidOutput);
-    }
-
-    public double getVelocity() {
-        return leftShooterMotor.getSelectedSensorVelocity();
-    }
-
-    public double getError() {
-        return desiredVelocity - getVelocity();
-    }
-
-    public void requestState(ShooterStates requestedState) {
-        m_shooterRequestedState = requestedState;
-    }
-
-    public ShooterStates getCurrentState() {
-        return m_shooterCurrentState;
-    }
-}
+    
