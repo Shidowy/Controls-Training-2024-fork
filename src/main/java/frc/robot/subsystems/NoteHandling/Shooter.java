56e833ba4b3fac43075f085cf48eaaa6e7252af6
ImBlobbyFish <rsen2028@francisparker.org>
2024-11-14 18:48:37 -0800

    testForWyatt

--- src/main/java/frc/robot/subsystems/NoteHandling/Shooter.java
+++ src/main/java/frc/robot/subsystems/NoteHandling/Shooter.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.ShooterConstants.*;
 
 import java.util.function.DoubleSupplier;
-
+ 
 import static frc.robot.Constants.*;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.*;
