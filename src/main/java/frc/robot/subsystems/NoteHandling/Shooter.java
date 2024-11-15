869a14e471ccf4ab44e01252dd0f1a35e31e1aad
Shidowy <101477459+Shidowy@users.noreply.github.com>
2024-11-14 18:53:51 -0800

    ONG BBG

--- src/main/java/frc/robot/subsystems/NoteHandling/Shooter.java
+++ src/main/java/frc/robot/subsystems/NoteHandling/Shooter.java
@@ -46,13 +46,22 @@ public class Shooter extends SubsystemBase {
 
         // CREATE THE CONFIGURATIONS FOR THE TALONS HERE
         // talon configs are set up differently than sparks, please use the doc if you want to spare your sanity
-        var talonFXConfigs = new TalonFXConfiguration();
-        
-        // ||||||||||||||||||||||||||||||||
-
-        // give some default state to these guys
-        // m_shooterCurrentState;
-        // m_shooterRequestedState;
+        TalonFXConfiguration talonFXConfigs = new TalonFXConfiguration();
+    
+        talonFXConfigs.MotorOutput = new MotorOutput();
+        talonFXConfigs.MotorOutput.neutralMode = NeutralMode.Coast;
+        talonFXConfigs.MotorOutput.inverted = InvertType.None;
+    
+        talonFXConfigs.CurrentLimits = new CurrentLimitConfiguration();
+        talonFXConfigs.CurrentLimits.enable = true;
+        talonFXConfigs.CurrentLimits.currentLimit = 40;
+    
+        talonFXConfigs.Feedback = new FeedbackConfiguration();
+        talonFXConfigs.Feedback.sensorType = FeedbackDevice.IntegratedSensor;
+        talonFXConfigs.Feedback.sensorCoefficient = 1.0;
+    
+        TalonFX talonFXMotor = new TalonFX(1);
+        talonFXMotor.configAllSettings(talonFXConfigs);
 
     }
         
